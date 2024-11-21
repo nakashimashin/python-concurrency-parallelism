4b0cc58f225426b819ec61ea5a257a4362e8f243
nakashimashin <nakashimashin0517@gmail.com>
2024-11-21 23:36:46 +0900

    add: pythonで並行処理を実装

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print("hello world")
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+def func_1():
+    for n in range(3):
+        time.sleep(2)
+        print(f'func_1: {n}')
+    return '結果1'
+
+def func_2():
+    for n in range(3):
+        time.sleep(1)
+        print(f'func_2: {n}')
+    return '結果2'
+
+def main():
+    print("Start")
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future_1 = executor.submit(func_1)
+        future_2 = executor.submit(func_2)
+
+    result_1 = future_1.result()
+    result_2 = future_2.result()
+
+    print(f'result_1: {result_1}')
+    print(f'result_2: {result_2}')
+
+    print("End")
+
+if __name__ == '__main__':
+    main()
